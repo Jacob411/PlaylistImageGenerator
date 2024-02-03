1bb8b2d701dd2f8ba2c504377df646cd41a55bc1
Jacob Simmons <jakerstx@gmail.com>
2024-02-03 15:38:24 -0600

    move main into spotify data .py

--- spotify_utils.py
+++ spotify_utils.py
@@ -393,26 +393,7 @@ def get_all_features(access_token):
 #     json.dump(all_tracks, file, indent=4)  # indent=4 for pretty formatting
 
 # print("JSON data written to", output_file)
-code = authorize_spotify()
-token = get_access_token(code)
-playlist_hrefs = fetch_user_playlist_ids(token)
-print(playlist_hrefs)
-playlist_href = playlist_hrefs[6]
-playlist_href = 'https://api.spotify.com/v1/playlists/49jW4Nm4xKaPbDqAHPSySY'
-track_id = '4hm33jVolpa22nzhlY72jD'
-prompt_info = get_prompt_info_from_playlist(playlist_href, token)
-artist = get_track(track_id, token)['artists'][0]['id']
-print(prompt_info)
-genre_map = {}
-for prompt in prompt_info:
-    for genre in prompt['genres']:
-        if genre in genre_map:
-            genre_map[genre] += 1
-        else:
-            genre_map[genre] = 1
-#print sorted genre map
-print(sorted(genre_map.items(), key=lambda x: x[1], reverse=True))
-# print(track)
+#print(track)
 # TODO combine functions
 # TODO make sure no duplicates
 # TODO implement a public only option, that doesnt require auth
