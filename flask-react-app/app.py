ba22d7705545efc543dcf6431cb5f4a8bae95ab6
Jacob Simmons <jakerstx@gmail.com>
2024-02-04 01:21:01 -0600

    update apis

--- flask-react-app/app.py
+++ flask-react-app/app.py
@@ -1,4 +1,4 @@
-
+from spotify_utils import *
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello from Flask API!'
+
+@app.route('/get_playlist_image/<token>/<playlist_href>')
+def get_playlist_image(token, playlist_href):
+    print(f"Token: {token}")
+    print(f"Playlist href: {playlist_href}")
+    prompt_info = get_prompt_info_from_playlist(playlist_href, token)
+    print(prompt_info)
+    genre_map = {}
+    for prompt in prompt_info:
+        for genre in prompt['genres']:
+            if genre in genre_map:
+                genre_map[genre] += 1
+            else:
+                genre_map[genre] = 1
+    #print sorted genre map
+    print(sorted(genre_map.items(), key=lambda x: x[1], reverse=True))
+    #
