7afc98e2d324a6fb650e13cdfc1251728d826e42
Jacob Simmons <jakerstx@gmail.com>
2024-02-04 05:05:49 -0600

    yeat

--- flask-react-app/app.py
+++ flask-react-app/app.py
@@ -1,6 +1,8 @@
 from spotify_utils import *
+import requests
+from urllib.request import urlopen
 from flask import Flask, request
-from mongo import *
+from imageAPI import upload_image 
 from download import download_image 
 from main import get_image_and_description 
 app = Flask(__name__)
@@ -9,7 +11,7 @@ app = Flask(__name__)
 def hello():
     return 'Hello from Flask API!'
 
-# BQCeKbNc4oGiu9cdXAk_89Dbq64wWlM_N-MIAuLgWinv44i971JsnHp09DKvg8ntSQzQvxMqOtlginK4b33R4w6mqd9tpRitPL9oCBECC5cGyivSiktDCO4MA1iQ0lfo9ODDX8bqUMTbAZUu2EpqA8tobbZ6WeSDNDEeAyyj8avgnY5qk8EYw7zsmCnc11Oz4OlB8p7RsrxXNqkhf0d4GAt0gMYtoExv_GDnSx10n6e97J5HDH-7-Se3NSgCKpgaw80ps4cUQw
+# BQD1FxdQBoUGTzSNZoG6YHmwIPCef7Y-GadY481C4uMRsIt1aCDJgwXl1lk_iCQyLiS9HAygOus6MTJr3hmN7BT0zVpN9AcoZZ8dXc0QdogHP5Kg8C_cobOxXZx44UDscX5tkpYE-nDXLeIknlij4mWx7eTBc8fC1298vTig8SMtvUoIC5iAMnNTpQxnjNNS0-CB3VeLtQPAeDEYiXJdVauXjLZx9eCu7tSv70WxDjeV4rKz2G461LnuQgd-dOUVPGqYffL-og
 # href
 # https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n
 @app.route('/get_playlist_image', methods=['POST'])
@@ -37,9 +39,21 @@ def get_playlist_image():
     # strip out the count and leave only the genre
     top_5_genres = [genre[0] for genre in top_5_genres]
     print(top_5_genres)
-    image_url, description = get_image_and_description(top_5_genres)
+    description, image_url = get_image_and_description(top_5_genres)
 
     print(f"Image URL: {image_url}")
     print(f"Description: {description}")
+        
+     
+       # Open the image file from the URL in binary mode
+    with urlopen(image_url) as response:
+        image_data = response.read()
+    # write the image data to a temporary file
+    image_path = 'temp.png'
+    with open(image_path, 'wb') as f:
+        f.write(image_data)
+
+
+
     
     return { image_url: image_url, description: description }
